50ad66dc25da141d3c703f8b8ebeaad6fec336f8
Robiya <77631787+Robiyaxon@users.noreply.github.com>
2022-06-20 20:14:38 +0500

    push

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import { MainTopHeader } from './component/topheader/MainTopHeader';
+import { TopHeader } from './component/top_header/TopHeader';
 function App() {
   return (
     <div className="App">
-      salom dunyo!
+      <MainTopHeader />
+      <TopHeader/>
     </div>
   );
 }
---
+++ src/component/topheader/MainTopHeader.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import style from "./MainHeader.module.css"
+import time from "../../accses/MainHeader/time.png"
+import tel from "../../accses/MainHeader/tel.png"
+import email from "../../accses/MainHeader/email.png"
+export const MainTopHeader = () => {
+  return (
+    <div className={style.TopHeaderMain}>
+      <div className={style.Network}>
+        <a href="/"><img src={time} alt="rasm bor edi!" /> Sun-Fri (9am-5pm)</a>
+        <a href="tel:+88(019)24-184 461"><img src={tel} alt="rasm bor edi!" /> +88(019)24-184 461</a>
+        <a href="mailto: shovasatkhira@gmail.com"><img src={email} alt="rasm bor edi!" /> shovasatkhira@gmail.com</a>
+
+      </div>
+
+    </div>
+  )
+}
