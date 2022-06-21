d74b4821128a46d9d869bc6cad16d4f2b2366832
Robiya <77631787+Robiyaxon@users.noreply.github.com>
2022-06-21 17:48:06 +0500

    push

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css';
-import { Doctors } from './component/doctors/Doctors';
 import { Footer } from './component/footer/Footer';
-import { Service } from './component/serveices/Service';
-
 import { MainTopHeader } from './component/topheader/MainTopHeader';
-
 function App() {
   return (
     <div className="App">
       <MainTopHeader />
-   
       <Footer/>
     </div>
   );
