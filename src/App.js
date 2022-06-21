d6be399e80413b553c463228f607ae51b413b02e
Robiya <77631787+Robiyaxon@users.noreply.github.com>
2022-06-21 19:37:04 +0500

    pull

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Footer } from './component/footer/Footer';
 import { MainTopHeader } from './component/topheader/MainTopHeader';
-import  { MyBackTop } from './ScrollerOn';
+import { MyBackTop } from './ScrollerOn';
+import Login from './component/login/Login';
 function App() {
   return (
     <div className="App">
       <MainTopHeader />
-      <Footer/>
+      <Footer />
       <MyBackTop />
+      <Routes>
+        <Route path="login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
